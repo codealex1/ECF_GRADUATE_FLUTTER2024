2b6fee44b83c93f17f5f95a5f4785202fcb5bb2e
codealex1 <alex2005.brunet@gmail.com>
2024-07-05 12:13:41 +0200

    page connexion

--- FrontEnd/Router/allRoutes.js
+++ FrontEnd/Router/allRoutes.js
@@ -5,7 +5,8 @@ export const allRoutes = [
     new Route("/FrontEnd", "Accueil", "/FrontEnd/pages/home.html"),
     new Route("/FrontEnd/services", "services", "/FrontEnd/pages/services.html"),
     new Route("/FrontEnd/habitats", "habitats", "/FrontEnd/pages/habitats.html"),
-    new Route("/FrontEnd/contact", "contact", "/FrontEnd/pages/contact.html"),];
+    new Route("/FrontEnd/contact", "contact", "/FrontEnd/pages/contact.html"),
+    new Route("/FrontEnd/connexion", "connexion", "/FrontEnd/pages/connexion.html"),];
 
 
 
